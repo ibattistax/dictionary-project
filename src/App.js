7b09a4eae349947370b4e314d08ea29e6bdae855
ibattistax <isabel.battista@gmail.com>
2024-11-05 14:41:53 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Dictionary</h1>
+      <p>Hello this is a dictionary</p>
+    </div>
+  );
+}
+
+export default App;
