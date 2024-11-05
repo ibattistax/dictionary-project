805b5b13934f9ca1ed0e285aa6ee6362c0a5f9ba
ibattistax <isabel.battista@gmail.com>
2024-11-05 16:17:26 +0000

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,9 +3,19 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response);
+  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for the meaning of ${keyword}`);
+
+    // API: https://www.shecodes.io/learn/apis/dictionary
+    let apiKey = "oaff0b06238ce15e1bda3c4dt5f4a7ba";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
