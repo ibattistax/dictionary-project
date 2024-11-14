db34ddbc54ad9b455769932b5af902f2fb5faa80
ibattistax <isabel.battista@gmail.com>
2024-11-14 11:32:49 +0000

    Fixed axios bug, displayed results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response);
-  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for the meaning of ${keyword}`);
@@ -14,9 +14,13 @@ export default function Dictionary() {
     // API: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "oaff0b06238ce15e1bda3c4dt5f4a7ba";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-
     axios.get(apiUrl).then(handleResponse);
   }
+  function handleResponse(response) {
+    setResults(response.data);
+    console.log(response);
+  }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
@@ -30,6 +34,7 @@ export default function Dictionary() {
           onChange={handleKeywordChange}
         ></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
