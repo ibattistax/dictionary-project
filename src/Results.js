83525d65fbb4260ea707bdd49764c14c23dcc2ee
ibattistax <isabel.battista@gmail.com>
2024-11-14 12:15:09 +0000

    Added display of live results

--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
+  console.log(props.results);
   if (props.results) {
-    return <div className="Results">Hello from results</div>;
+    return (
+      <div uclassName="Results">
+        <h2>{props.results.word}</h2>
+        <p>{props.results.phonetic}</p>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
